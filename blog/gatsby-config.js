a102e513fc1d652fdd6c3b7e81e6d20e3ae97f63
Jennifer Mackown <jen@scholarpack.com>
2018-11-07 08:41:07 +0000

    upgraded and added contentful info

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,3 +1,10 @@
+// environemnt variables
+
+// require("dotenv").config({
+//   path: `.env.${process.env.development}`,
+// })
+
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
@@ -25,8 +32,30 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+
+
+    // Contentful
+
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `ccp7n98trjxu`,
+        accessToken: `6106a667817965026820e2917a01ab7cbddf6b8d2e18954100598c5f4eff2e3a`,
+      },
+    },
+
+    // {
+    //   resolve: `gatsby-source-contentful`,
+    //   options: {
+    //     spaceId: `fs7ksa3tw5d1`,
+    //     accessToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+    //     host: `preview.contentful.com`,
+    //   },
+    // },    
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
   ],
+
 }
